e161c82b26abc55d260e7165117a7d75aecb7d2a
vedant <vedantraval30@gmail.com>
2024-08-10 21:49:56 +0530

    added loading packman

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-52" src="packman.svg" alt="loading..." />
+    </div>
+  );
+}
